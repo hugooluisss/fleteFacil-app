60e6818d87bf679b9c72da7781d72ff1e4a3eff4
Hugo Luis Santiago Altamirano <hugo.santiago@iebo.edu.mx>
2017-07-14 12:16:24 -0500

    Forma de inicio de la aplicación

--- js/inicio.js
+++ js/inicio.js
@@ -137,7 +137,7 @@ var app = {
 	}
 };
 
-//app.initialize();
+app.initialize();
 
 $(document).ready(function(){
 	//$("body").css("height", $(window).height());
@@ -147,5 +147,5 @@ $(document).ready(function(){
 		$("#modulo").css("height", $(window).height() - $(".navbar-fixed-top").height() - $("#menu").height() - 13);
 	});
 	
-	app.onDeviceReady();
+	//app.onDeviceReady();
 });
