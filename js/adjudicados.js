c0a2b0e475bc0e84d714d5cb0f6570f39ce25641
Hugo Luis Santiago Altamirano <hugo.santiago@iebo.edu.mx>
2017-12-15 13:12:53 -0600

    Actualizando y forzando a 1 minuto la actualización

--- js/adjudicados.js
+++ js/adjudicados.js
@@ -291,7 +291,7 @@ function panelAdjudicados(){
 				
 				if (idOrden != undefined && idOrden != ''){
 					var ultimoUpdate = new Date(fecha);
-					if (dt.getTime() - (60 * 1000) <= fecha){
+					if (dt.getTime() - (60 * 8640) <= fecha){
 						window.localStorage.setItem("fecha", ultimoUpdate.getTime());
 						
 						$.post(server + 'cordenes', {
